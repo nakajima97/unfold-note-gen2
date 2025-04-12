da9e98d9be7aceb28cbf3837c0e18d0b247420be
nakajima97 <kituneudon97@gmail.com>
2025-04-12 11:19:50 +0900

    #72 不要な表記を削除

--- frontend/src/app/(private)/projects/[projectUrlId]/notes/layout.tsx
+++ frontend/src/app/(private)/projects/[projectUrlId]/notes/layout.tsx
@@ -8,7 +8,6 @@ const NotesLayout = ({ children }: NotesLayoutProps) => {
   return (
     <div className="container mx-auto p-4">
       <div className="flex flex-col gap-4">
-        <h1 className="text-2xl font-bold">Notes</h1>
         {children}
       </div>
     </div>
