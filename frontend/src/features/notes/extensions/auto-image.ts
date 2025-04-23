a1a6dc674dd371f1a8e22232b841ecaaf958938f
nakajima97 <kituneudon97@gmail.com>
2025-04-23 09:40:24 +0900

    #117 コメント追記等のリファクタリング

--- frontend/src/features/notes/extensions/auto-image.ts
+++ frontend/src/features/notes/extensions/auto-image.ts
@@ -1,7 +1,12 @@
 import { Extension, InputRule } from '@tiptap/core';
 import { Plugin, Selection } from 'prosemirror-state';
 
-function imageUrlInputRule(regexp: RegExp) {
+/**
+ * URL形式の画像を検出するInputRule
+ * @param regexp URL正規表現
+ * @returns InputRule
+ */
+const imageUrlInputRule = (regexp: RegExp) => {
   return new InputRule({
     find: regexp,
     handler: ({ state, range, match, commands }) => {
@@ -15,8 +20,12 @@ function imageUrlInputRule(regexp: RegExp) {
   });
 }
 
+// URL形式の画像を検出する正規表現
 const imageUrlRegExp = /https?:\/\/[\w\-._~:/?#[\]@!$&'()*+,;=%]+\.(jpg|jpeg|png|gif|webp)(?:\?[^\\s]*)?/gi;
 
+/**
+ * URL形式の画像を検出して自動的に画像ノードを挿入する拡張
+ */
 const AutoImage = Extension.create({
   name: 'autoImage',
   addInputRules() {
